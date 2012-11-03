ab30bc508f04fd2d767f8f519aee0a0b26caa44b
PeeHaa <info@pieterhordijk.com>
2012-11-03 18:19:04 +0100

    added Authentication class

---
+++ Application/public/js/Authentication.js
@@ -0,0 +1,72 @@
+'use strict';
+
+function Authentication() {
+
+}
+
+Authentication.prototype.showPopup = function(popup, loginButton) {
+    if (popup.isActive('login')) {
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState < 4 || xhr.status !== 200) {
+            return;
+        }
+
+        if (xhr.readyState === 4) {
+            var popupElement = popup.show('login', xhr.responseText);
+            popupElement.querySelector('input[name="username"]').focus();
+        }
+    };
+
+    xhr.open('GET', loginButton.href, true);
+    xhr.send();
+};
+
+Authentication.prototype.login = function(form) {
+    var formValues = $(form).getFormValues();
+
+    if (!formValues['username'] || !formValues['password']) {
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState < 4 || xhr.status !== 200) {
+            return;
+        }
+
+        if (xhr.readyState === 4) {
+            var data = JSON.parse(xhr.responseText);
+
+            if (typeof data.result === 'undefined' || data.result != 'success') {
+                this.invalidateForm(form);
+             } else {
+                location.reload(true);
+            }
+        }
+    }.bind(this);
+
+    xhr.open(form.method, form.action, true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(formValues.buildQueryString());
+};
+
+Authentication.prototype.invalidateForm = function(form) {
+    var username = form.querySelector('input[name="username"]'),
+        password = form.querySelector('input[name="password"]');
+
+    if (!$(username).hasClass('error')) {
+        $(username).addClass('error');
+    }
+    if (!$(password).hasClass('error')) {
+        $(password).addClass('error');
+    }
+
+    password.value = '';
+
+    username.blur();
+    password.blur();
+};
