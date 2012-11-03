ffd82a6f16f4c89ea4b3bb5b9e162bc68d62651f
PeeHaa <info@pieterhordijk.com>
2012-11-03 18:18:52 +0100

    added Popup class

---
+++ Application/public/js/Popup.js
@@ -0,0 +1,45 @@
+function Popup() {
+    this.activeType = null;
+}
+
+Popup.prototype.isActive = function(type) {
+    var popup = this.get();
+
+    if (popup === null || (typeof type !== 'undefined' && this.activeType != type)) {
+        return false;
+    }
+
+    return true;
+};
+
+Popup.prototype.show = function(type, content) {
+    this.activeType = type;
+
+    var body  = document.getElementsByTagName('body')[0],
+        popup = document.createElement('div');
+
+    $(document.getElementById('body')).addClass('inactive');
+    $(popup).addClass('popup');
+
+    popup.innerHTML = content;
+    body.appendChild(popup);
+
+    $(popup).center();
+
+    return popup;
+};
+
+Popup.prototype.remove = function() {
+    var popup = this.get();
+
+    if (popup === null) {
+        return;
+    }
+
+    document.querySelector('body').removeChild(popup);
+    $(document.getElementById('body')).removeClass('inactive');
+};
+
+Popup.prototype.get = function() {
+    return document.querySelector('.popup');
+};
