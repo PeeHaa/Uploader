efff5050a8b80784a2743ffc16bfa19ef4042d07
PeeHaa <info@pieterhordijk.com>
2012-11-07 00:06:29 +0100

    added permissions to routes

--- Application/routes.php
+++ Application/routes.php
@@ -11,26 +11,47 @@ $routes = [
         'requirements' => [
             'path'   => '/^(\/?login\/?)$/',
             'method' => 'post',
+            'permissions' => [
+                'match' => 'guest',
+            ],
         ],
     ],
     'user/login/popup' => [
         'requirements' => [
             'path'   => '/^(\/?login-popup\/?)$/',
             'method' => 'get',
+            'permissions' => [
+                'match' => 'guest',
+            ],
         ],
     ],
     'user/logout' => [
         'requirements' => [
             'path'   => '/^(\/?logout\/.+\/?)$/',
             'method' => 'get',
+            'permissions' => [
+                'minimum' => 'user',
+            ],
         ],
         'mapping' => [
             'csrf-token' => 1,
         ],
     ],
+    'user/settings' => [
+        'requirements' => [
+            'path'   => '/^(\/?settings\/?)$/',
+            'method' => 'get',
+            'permissions' => [
+                'minimum' => 'user',
+            ],
+        ],
+    ],
     'upload' => [
         'requirements' => [
             'path' => '/^(\/?upload\/)/',
+            'permissions' => [
+                'minimum' => 'user',
+            ],
         ],
     ],
 ];
