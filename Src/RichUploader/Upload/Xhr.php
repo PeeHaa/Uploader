cec2d1d9dbb2ee355f5bd6974d39a75e45533ccf
PeeHaa <info@pieterhordijk.com>
2012-12-30 17:48:13 +0100

    change double quotes to single for consitency

--- Src/RichUploader/Upload/Xhr.php
+++ Src/RichUploader/Upload/Xhr.php
@@ -93,8 +93,8 @@ class Xhr implements Uploadable
      */
     public function getSize()
     {
-        if (isset($_SERVER["CONTENT_LENGTH"])){
-            return (int)$_SERVER["CONTENT_LENGTH"];
+        if (isset($_SERVER['CONTENT_LENGTH'])){
+            return (int)$_SERVER['CONTENT_LENGTH'];
         } else {
             throw new \Exception('Getting content length is not supported.');
         }
